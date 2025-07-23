5fb2f72ca8564f495d278bdafd2cac45dc6deaac
Jayamal <jayamal.jayamaha.x@gmail.com>
2025-07-23 22:00:11 +0530

    feat: new changes with cache

--- src/services/auth/signin/SigninService.ts
+++ src/services/auth/signin/SigninService.ts
@@ -1,13 +1,14 @@
-
 import ISigninService from "./ISigninService";
 import { UserData, UserSigninPayload } from "../../../utils/types/common/types";
 import DataSourceConfig from "../../../config/DataSourceConfig";
 import configs from "../../../config/configs";
 import Fuse from "fuse.js";
 import CommonValidator from "../../../utils/validators/CommonValidator";
+import NodeCache from "node-cache";
 
 class SigninService implements ISigninService {
   private static instance: ISigninService = new SigninService();
+  private cache: NodeCache = new NodeCache({ stdTTL: 60 * 60 });
 
   private constructor() {}
 
@@ -16,20 +17,24 @@ class SigninService implements ISigninService {
   }
 
   async userSignIn(payLoad: UserSigninPayload): Promise<Array<UserData>> {
-    CommonValidator.validateNotEmptyOrBlankString(payLoad.userName, "user name", true)
-    const userData: Promise<Array<UserData>> = DataSourceConfig.getUserData(configs.userDataURL)
-    return userData.then(userData => this.userSearch(payLoad.userName, userData))
+    CommonValidator.validateNotEmptyOrBlankString(payLoad.userName, "user name", true);
+    let userData: Promise<Array<UserData>> | undefined = this.cache.get("userData");
+    if (userData === undefined) {
+      userData = DataSourceConfig.getUserData(configs.userDataURL);
+      this.cache.set("userData", userData);
+      console.log("Cache miss");
+    }
+    return userData.then((userData) => this.userSearch(payLoad.userName, userData));
   }
 
   private userSearch(term: string, userData: Array<UserData>): Array<UserData> {
-     const fuse = new Fuse(userData, {
+    const fuse = new Fuse(userData, {
       keys: ["userName"],
       threshold: 0.4,
     });
     const results = fuse.search(term);
-    return results.map(result => result.item)
+    return results.map((result) => result.item);
   }
-
 }
 
 export default SigninService;
